34a2b3dbcff441029b7fad3a012762ca418b2cfe
Valetin Belousow <bel2002v@yandex.ru>
2023-03-06 13:40:32 +0300

    login repair

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -1,20 +1,36 @@
 import Client from "../models/Client.js";
+import Admin from "../models/Admin.js";
 
-export const login = async (req, res) => {
+const login = async (req, res) => {
     const { email, password } = req.body;
 
     try {
-// Поиск клиента в базе данных по email и password
+        // Поиск клиента в базе данных по email и password
         const client = await Client.findOne({ where: { email, password } });
-        if (!client) {
-            res.status(401).send("Неправильный логин или пароль");
+        if (client) {
+            // Установка cookie с информацией о том, что клиент аутентифицирован
+            res.cookie("authenticated", "true");
+            res.cookie("clientEmail", email);
+
+            // Возвращение JSON для клиента
+            res.json({ role: "client", email, password });
             return;
         }
-// Установка cookie с информацией о том, что клиент аутентифицирован
-        res.cookie("authenticated", "true");
-        res.cookie("clientEmail", email);
 
-        res.send("Вы успешно вошли в систему!");
+        // Поиск админа в базе данных по email и password
+        const admin = await Admin.findOne({ where: { email, password } });
+        if (admin) {
+            // Установка cookie с информацией о том, что админ аутентифицирован
+            res.cookie("authenticated", "true");
+            res.cookie("adminEmail", email);
+
+            // Возвращение JSON для админа
+            res.json({ role: "admin", email, password });
+            return;
+        }
+
+        // Если пользователь не найден, возвращаем ошибку авторизации
+        res.status(401).send("Неправильный логин или пароль");
     } catch (err) {
         console.error(err);
         res.status(500).send("Ошибка сервера");
