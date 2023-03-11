97bf74ed93fa7819048ba89bcd5531e2c751a3f9
Valetin Belousow <bel2002v@yandex.ru>
2023-03-11 18:59:19 +0300

    fix: update client controller

--- server/controllers/clientController.js
+++ server/controllers/clientController.js
@@ -65,25 +65,25 @@ const createClient = async (req, res) => {
 
 const updateClient = async (req, res) => {
     const { id } = req.params;
-    const { firstName: first_name, lastName: last_name, email, phoneNumber: phone_number, password, dateOfBirth: date_of_birth, gender, address, registrationDate: registration_date, subscriptionStatus: subscription_status, subscriptionEndDate: subscription_end_date, availableSessions: available_sessions } = req.body;
+    const { firstName: firstName, lastName: lastName, email, phoneNumber: phoneNumber, password, dateOfBirth: dateOfBirth, gender, address, registrationDate: registrationDate, subscriptionStatus: subscriptionStatus, subscriptionEndDate: subscriptionEndDate, availableSessions: availableSessions } = req.body;
     try {
         const client = await Client.findByPk(id);
         if (!client) {
             return res.status(404).json({ message: 'Client not found' });
         }
         const updatedClient = await client.update({
-            first_name,
-            last_name,
+            firstName,
+            lastName,
             email,
-            phone_number,
+            phoneNumber,
             password,
-            date_of_birth,
+            dateOfBirth,
             gender,
             address,
-            registration_date,
-            subscription_status,
-            subscription_end_date,
-            available_sessions
+            registrationDate,
+            subscriptionStatus,
+            subscriptionEndDate,
+            availableSessions
         });
         res.status(200).json(updatedClient);
     } catch (error) {
